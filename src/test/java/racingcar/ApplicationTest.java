ee7e845b19b168f569310ee178a26b692c714a38
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-10-26 23:31:44 +0900

    test: 정상 및 입력 예외 케이스 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,44 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 여러_자동차_동시_우승_테스트() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni,jun", "1");
+                assertThat(output()).contains("pobi : -", "woni : -", "jun : -", "최종 우승자 : pobi, woni, jun");
+            },
+            MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 여러_라운드_진행_테스트() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("pobi,woni", "3");
+                assertThat(output()).contains("pobi : ", "woni : ", "최종 우승자 : ");
+            },
+            MOVING_FORWARD, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 시도_횟수_음수_예외_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni", "-1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 자동차_이름_빈값_예외_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
